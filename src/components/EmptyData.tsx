c0e36d4ced31f2ca974276d11e97ad8403a14e05
work4m <workingformix@gmail.com>
2023-06-30 20:24:55 +0530

    empty view with logo

---
+++ src/components/EmptyData.tsx
@@ -0,0 +1,11 @@
+import BG_LOGO from "./../assets/bg_logo.png";
+
+function EmptyData() {
+  return (
+    <div className="main-col-bg-logo-container">
+        <img src={BG_LOGO} className="main-col-bg-logo" />
+    </div>
+  )
+}
+
+export default EmptyData
--- src/components/MainCol.tsx
+++ src/components/MainCol.tsx
@@ -1,12 +1,26 @@
+import { useSelector } from "react-redux";
+
+import { RootState } from "../store/Store";
+import EmptyData from "./EmptyData";
 
 function MainCol() {
+    const notes = useSelector((state: RootState) => state.notes);
+
+    const { selectedCategory, selectedNote, note_data } = notes;
+
     return (
         <div
             className='column main-column'
         >
-            <textarea className="main-col-textarea">
-                
-            </textarea>
+            {
+                (selectedCategory == null || selectedNote == null)
+                    ?
+                    <EmptyData />
+                    :
+                    <textarea className="main-col-textarea" placeholder="Enter text here">
+                        {note_data[selectedCategory]?.content[selectedNote]?.description}
+                    </textarea>
+            }
         </div>
     )
 }
